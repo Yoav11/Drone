543277aa0109454fa27ad2a286cb15ce3c31036a
yoav nir <11yoav.nir@gmail.com>
2018-07-29 16:14:37 +0100

    failed attempt at MPU values

--- Drone.ino
+++ Drone.ino
@@ -1,7 +1,3 @@
-// MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
-// Public Domain
 
 void setup() {
   setupMPU();
--- MPUBoard.ino
+++ MPUBoard.ino
@@ -1,6 +1,8 @@
+//the used MPU is a GY521
 #include<Wire.h>
 const int MPU_addr = 0x68; // I2C address of the MPU-6050
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+float AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+float R,Rx,Ry,Rz,aRx,aRy,aRz;
 
 void setupMPU() {
   Wire.begin();
@@ -15,20 +17,39 @@ void loopMPU() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  Wire.requestFrom(MPU_addr, 6, true); // request a total of 14 registers, in order: acc x,y,z; Temp; Gyro x,y,z
   AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
+
+  Rx = AcX/2048;
+  Ry = AcY/2048;
+  Rz = AcZ/2048;
+
+  R = sqrt(sq(Rx)+sq(Ry)+sq(Rz));
+  aRx = acos(Rx/R)*57296 / 1000;
+  aRy = acos(Ry/R)*57296 / 1000;
+  aRz = acos(Rz/R)*57296 / 1000;
+  
+  Serial.print("AcX = "); Serial.print(aRx);
+  Serial.print(" | AcY = "); Serial.print(aRy);
+  Serial.print(" | AcZ = "); Serial.println(aRz);
+
   delay(333);
 }
+
+
+
+//  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+//  Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
+//  Serial.print(" | GyX = "); Serial.print(GyX);
+//  Serial.print(" | GyY = "); Serial.print(GyY);
+//  Serial.print(" | GyZ = "); Serial.println(GyZ);
+
+
+
+
